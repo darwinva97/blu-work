f046f2a523c706f18e524dc610b81dec090dead3
dsva <darwin97.va@gmail.com>
2023-07-18 19:31:34 -0500

    ready

---
+++ src/app/api/email/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export async function POST(req: Request) {
+  const { name, email, feedback } = await req.json();
+  const info = await transporter.sendMail({
+    from: `"${process.env.SMTP_NAME}" <${process.env.SMTP_USER}>`, // sender address
+    to: `${email}, ${process.env.SMTP_USER}`, // list of receivers
+    subject: `Formulario de Feedback`, // Subject line
+    text: `Feedback enviado por ${name} (${email}) \n "${feedback}"`, // plain text body
+    html: /*html*/ `
+      <h1>Feedback enviado por ${name} (${email})</h1>
+      <p>${feedback}</p>
+    `,
+  });
+  if (info.messageId) {
+    return NextResponse.json({
+      message: `Email enviado com sucesso!`,
+    });
+  } else {
+    return NextResponse.json({
+      message: `Error al enviar el email!`,
+    });
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,155 @@
-import Image from 'next/image'
+"use client";
+import { Formik } from "formik";
 
-export default function Home() {
+type TInputProps = {
+  label: string;
+  error?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = ({ label, required, error, ...props }: TInputProps) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="form-control w-full">
+      <label className="label">
+        <span className="label-text">
+          {label} {required && "*"}
+        </span>
+      </label>
+      <input className="input input-bordered w-full" {...props} />
+      <label className="label">
+        {error && <span className="label-text-alt">{error}</span>}
+      </label>
+    </div>
+  );
+};
+
+type TTextareaProps = {
+  label: string;
+  error?: string;
+} & React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const Textarea = ({ label, required, error, ...props }: TTextareaProps) => {
+  return (
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">
+          {label} {required && "*"}
+        </span>
+      </label>
+      <textarea
+        className="textarea textarea-bordered h-24"
+        {...props}
+      ></textarea>
+      <label className="label">
+        {error && <span className="label-text-alt">{error}</span>}
+      </label>
+    </div>
+  );
+};
+
+type TDataForm = {
+  name: string;
+  email: string;
+  feedback: string;
+};
+
+const Page = () => {
+  return (
+    <div className="h-[100vh] w-full flex bg-yellow-300 justify-center items-center">
+      <Formik
+        initialValues={{ name: "", email: "", feedback: "" }}
+        validate={(values) => {
+          const errors: Partial<TDataForm> = {};
+
+          Object.entries(values).forEach(([key, value]) => {
+            if (value === "") {
+              errors[key as keyof TDataForm] = "Este campo es requerido";
+            }
+          });
+          if (!values.email) {
+            errors.email = "Este campo es requerido";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "El correo electrónico es inválido";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
+          fetch("/api/email", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(values),
+          })
+            .then((res) => res.json())
+            .then((res) => alert(res.message))
+            .finally(() => {
+              setSubmitting(false);
+              resetForm();
+            });
+        }}
+      >
+        {({
+          values,
+          errors,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col items-stretch p-[5rem]"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            <h1 className="text-2xl font-bold text-center max-w-md">
+              AYUDANOS A LANZAR LA MEJOR VERSIÓN DE BLU WORK
+            </h1>
+            <Input
+              label="Nombre"
+              name="name"
+              required
+              placeholder="ejm: Juan Gonzáles"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.name}
+              error={errors.name}
             />
-          </a>
-        </div>
-      </div>
+            <Input
+              label="Correo"
+              name="email"
+              required
+              placeholder="ejm: user@gmail.com"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+              error={errors.email}
+            />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <Textarea
+              label="Feedback"
+              name="feedback"
+              required
+              placeholder="ejm: Me encanta su plataforma!"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.feedback}
+              error={errors.feedback}
+            />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="btn btn-primary"
+            >
+              {isSubmitting ? "Enviando..." : "Enviar"}
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Page;
